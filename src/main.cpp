a7713837a12d13bd7397e7a9c8bc03abb7fabd2b
Ian Hoang <134716435+ianhoangdev@users.noreply.github.com>
2025-10-29 19:38:44 -0400

    added total ads metrics

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,14 @@ void metrics_printer() {
         
         size_t current_reqs = g_requests_processed.load(std::memory_order_relaxed);
         size_t reqs_per_sec = (current_reqs - last_reqs) / 2;
+        auto snap = std::atomic_load(&current_index);
+        size_t total_ads = snap ? snap->ads_storage.size() : 0;
         last_reqs = current_reqs;
         
         std::cout << "\n--- METRICS (2s) ---\n"
                   << "QPS (approx):   " << reqs_per_sec << "\n"
                   << "Total Requests: " << current_reqs << "\n"
+                  << "Total Ads:      " << total_ads << "\n"
                   << "Total Matched:  " << g_ads_matched.load(std::memory_order_relaxed) << "\n"
                   << "Total No Match: " << g_no_match_found.load(std::memory_order_relaxed) << "\n---\n\n";
     }
